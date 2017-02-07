fde85b8efad2e5e775c630d88a9824bb3535a7ed
Luke O'Sullivan <hosullivan2004_1@hotmail.com>
2017-02-07 04:38:53 +0000

    Week 1, pset1, credit.
    Gets credit to the point where it will return the correct checksum.

--- pset1/credit.h
+++ pset1/credit.h
@@ -1,30 +1,31 @@
-//#include <cs50.h>
-
 bool test_cc(long long);
 long long get_credit_card();
+int reduce_10(int);
 
 bool test_cc(long long cc) {
     bool is_cc;
     int sum = 0;
     int mod = 0;
     int oddsum = 0;
+    int evensum = 0;
     
-    for (int i = 0; i<8; i++) {
+    for (int i = 0; i < 8; i++) {
         printf("cc is %lld\n", cc);
+        
         oddsum += cc % 10;
+        
         cc /= 10;
         mod = (cc % 10);
         cc /= 10;
-        if(mod * 2 > 10) {
-            sum = ((mod * 2) % 10) + (mod / 10);
-        }
-        else{
-            sum = (mod * 2);
-        }
+        
+        evensum += reduce_10(mod);
+        
         printf("mod is %d\n", mod);
         printf("oddsum is %d\n", oddsum);
-        printf("sum is %d\n", sum);
+        printf("evensum is %d\n", evensum);
     }
+    
+    sum = evensum + oddsum;
     
     printf("sum is %d", sum);
     is_cc = true;
@@ -39,4 +40,17 @@ long long get_credit_card() {
     
     cc_num = get_long_long();
     return cc_num;
+}
+
+int reduce_10(int num) {
+    int sum;
+    printf("reduce_10 num is %d; num * 2 is %d\n", num, num*2);
+    if(num * 2 > 10) {
+        sum = ((num * 2) % 10) + ( (num*2) / 10);
+    }
+    else {
+        sum = (num * 2);
+    }
+    printf("reduce_10 num returns %d\n", sum);
+    return sum;
 }
