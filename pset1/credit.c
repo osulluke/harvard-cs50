052457db536364e7c333846ce7a0b8c0b2f158ec
Luke O'Sullivan <hosullivan2004_1@hotmail.com>
2017-02-13 06:09:39 +0000

    Week 1, pset1, P4 (refactoring).
    Move the code that classifies the credit card as one of the big three into the functions file. Allows the main function to read very smoothly.

--- pset1/credit.c
+++ pset1/credit.c
@@ -9,33 +9,7 @@ int main(void) {
     int slim = trim(cc);
         
     if(test_cc(cc)) {
-        //printf("test_cc returned true!\n");
-        //printf("slim is %d\n", slim);
-        switch(slim) {
-            case 34:
-            case 37: 
-                printf("AMEX\n");
-                break;
-            case 55:
-            case 54:
-            case 53:
-            case 52:
-            case 51:
-                printf("MASTERCARD\n");
-                break;
-            case 49:
-            case 48:
-            case 47:
-            case 46:
-            case 45:
-            case 44:
-            case 43:
-            case 42:
-            case 41:
-            case 40:
-                printf("VISA\n");
-                break;
-        }
+        classify(slim);
     }
     else {
         printf("INVALID\n");
--- pset1/credit.h
+++ pset1/credit.h
@@ -2,6 +2,7 @@ bool test_cc(long long);
 long long get_credit_card();
 int reduce_10(int);
 int trim(long long);
+void classify(int);
 
 bool test_cc(long long cc) {
     bool is_cc = false;
@@ -68,4 +69,33 @@ int trim(long long num) {
     }
     
     return num;
+}
+
+void classify(int card) {
+    
+    switch(card) {
+        case 34:
+        case 37: 
+            printf("AMEX\n");
+            break;
+        case 55:
+        case 54:
+        case 53:
+        case 52:
+        case 51:
+            printf("MASTERCARD\n");
+            break;
+        case 49:
+        case 48:
+        case 47:
+        case 46:
+        case 45:
+        case 44:
+        case 43:
+        case 42:
+        case 41:
+        case 40:
+            printf("VISA\n");
+            break;
+    }
 }
