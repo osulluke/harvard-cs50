53656edc06bb2a26f22e4006832fcb391b602479
Luke OSullivan <osulluke@gmail.com>
2017-05-26 16:22:22 +0000

    Example of bad loop.

--- wk02/string0.c
+++ wk02/string0.c
@@ -8,7 +8,8 @@ int main(void)
     string s = get_string();
     //fix bug:
     if(s != NULL) 
-    {
+    {   
+        //poorly designed for loop, because it calls a function unecessarily
         for(int i = 0; i < strlen(s); i++) 
         {
             printf("%c\n", s[i]);
